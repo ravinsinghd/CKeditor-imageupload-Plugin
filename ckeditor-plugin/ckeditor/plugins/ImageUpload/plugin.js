e939046119035637c1e9f2621935115d45b8f74c
Ravin Singh D <ravinsinghd@outlook.com>
2016-10-06 10:57:22 +0530

    init commit

---
+++ ckeditor-plugin/ckeditor/plugins/ImageUpload/plugin.js
@@ -0,0 +1,56 @@
+CKEDITOR.plugins.add('ImageUpload', {
+    icons: 'ImageUpload',
+    init: function (editor) {
+
+        editor.addCommand('ImageUpload', new CKEDITOR.dialogCommand('ImageUploadDialog'));
+        editor.ui.addButton('ImageUpload', {
+            label: 'Insert Image',
+            command: 'ImageUpload',
+            toolbar: 'insert'
+        });
+        CKEDITOR.dialog.add('ImageUploadDialog', function (editor) {
+            return {
+                title: 'Upload Image',
+                minWidth: 400,
+                minHeight: 70,
+                contents: [
+                    {
+                        id: 'tab-basic',
+                        label: 'Basic Settings',
+                        elements: [
+                            {
+                                type: 'file',
+                                id: 'imgUpload',
+                                validate: CKEDITOR.dialog.validate.notEmpty("image field cannot be empty."),
+                                commit: function (element) {
+                                    fileInput = this.getInputElement().$;
+                                    file = fileInput.files[0];
+                                    /*CKEDITOR.ajax.post('http://localhost:63901/api/blob/assessmentImage', file,'', function (data) {
+                                        console.log(data);
+                                    });*/
+                                    var formData = new FormData();
+                                    formData.append(file.name, file);
+                                    var xhr = new XMLHttpRequest();
+                                    xhr.open('POST', 'http://localhost:63901/api/blob/assessmentImage', true);
+                                    xhr.onload = function (e) {
+                                        console.log(e);
+                                     };
+                                    xhr.send(formData);
+                                }
+                            }
+                        ]
+                    },
+                ],
+                onOk: function () {
+                    var dialog = this;
+                    var abbr = this.element;
+                    this.commitContent(abbr);
+
+                    if (this.insertMode)
+                        editor.insertElement(abbr);
+                }
+            };
+        });
+    }
+});
+
